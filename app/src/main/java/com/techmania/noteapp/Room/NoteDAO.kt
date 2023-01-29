a5ab0ae71056c44ab7df7715baedcfee1c2bf7fa
Tushar <tusharkansal14@gmail.com>
2023-01-29 12:18:29 -0800

    Initial Commit

---
+++ app/src/main/java/com/techmania/noteapp/Room/NoteDAO.kt
@@ -0,0 +1,29 @@
+package com.techmania.noteapp.Room
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.techmania.noteapp.Model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDAO {
+
+    @Insert
+    suspend fun insert(note : Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("DELETE FROM note_table")
+    suspend fun deleteAllNotes()
+
+    @Query("SELECT * FROM note_table ORDER BY id ASC")
+    fun getAllNotes() : Flow<List<Note>>
+
+}
